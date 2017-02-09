e78b83a6f7a9a7c16bab002895334c113ac14c98
Dejan Vjestica <dvjestica@h3ko.de>
2017-02-09 15:03:44 +0100

    jQuery ist eingesetzt, muss ich es weiterverarbeiten -Menu -Popup Listen
    CSS noch nicht fertig
    - bis Div#ID:Weiterbildung fertig
    - Mediaquery fält

--- js/myScript.js
+++ js/myScript.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+$(document).ready(function(){
+/* WINDOW RESIZE ACTIONS */
+  $(window).resize(function(){
+    fullscreen();
+
+  });
+
+/* WINDOW LOAD ACTIONS */
+
+  $(window).load(function(){
+    fullscreen();
+
+  });
+
+//fullsize sections
+	function fullscreen (){
+		$("header").css({
+			height: $(window).height() ,
+			width: $(window).width()
+		});
+  }
+
+
+});
