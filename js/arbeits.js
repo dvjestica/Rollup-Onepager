1d1b6fee1b2d7788db7702187afdb8566eec6d12
Dejan Vjestica <dvjestica@h3ko.de>
2017-02-13 11:23:29 +0100

    Safepoint-01

---
+++ js/arbeits.js
@@ -0,0 +1,33 @@
+// sections slidedown
+$(".welche-berufe-button-01").click(function(){
+    $(".hotel-list").slideToggle();
+  });
+
+$(".welche-berufe-button-02").click(function(){
+    $(".getränke-list").slideToggle();
+  });
+
+$(".welche-berufe-button-03").click(function(){
+    $(".lebensmittel-list").slideToggle();
+  });
+
+$(".welche-berufe-button-04").click(function(){
+    $(".haushalt-list").slideToggle();
+  });
+
+
+
+  <!-- Menu Icon  -->
+  <div class="menu-icon grid_1">
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+  </div>
+
+
+  <div id="nav-icon3 grid_1">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
--- js/myScript.js
+++ js/myScript.js
@@ -23,8 +23,38 @@ $(document).ready(function(){
 
   // sections slidedown
   $(".welche-berufe-button-01").click(function(){
-      $(".welche-berufe-list").slideToggle();
-    });
+    $(".hotel-list").fadeIn("slow");
+  });
+  $(".welche-berufe-button-05").click(function(){
+    $(".hotel-list").fadeOut("slow");
+  });
 
+  $(".welche-berufe-button-02").click(function(){
+    $(".getränke-list").fadeIn("slow");
+  });
+  $(".welche-berufe-button-06").click(function(){
+    $(".getränke-list").fadeOut("slow");
+  });
+
+  $(".welche-berufe-button-03").click(function(){
+    $(".lebensmittel-list").fadeIn("slow");
+  });
+  $(".welche-berufe-button-07").click(function(){
+    $(".lebensmittel-list").fadeOut("slow");
+  });
+
+  $(".welche-berufe-button-04").click(function(){
+    $(".haushalt-list").fadeIn("slow");
+  });
+  $(".welche-berufe-button-08").click(function(){
+    $(".haushalt-list").fadeOut("slow");
+  });
+
+
+  $(".banner-button-child").click(function(){
+    $("body").animate({
+      scrollTop: $(window).height()
+    });
+  });
 
 });
